86a70c41ac8266db0d56a14524c1a91a296d5d85
Rime Brika <rbrika2@uic.edu>
2019-04-26 16:41:17 +0000

    fixed caching issues and notifcations

--- sw.js
+++ sw.js
@@ -43,10 +43,17 @@ self.addEventListener('activate',  event => {
   event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request, {ignoreSearch:true}).then(response => {
-      return response || fetch(event.request);
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+          console.log('[Service Worker] Fetching resource: '+e.request.url);
+      return r || fetch(e.request).then(function(response) {
+                return caches.open(cacheName).then(function(cache) {
+          console.log('[Service Worker] Caching new resource: '+e.request.url);
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
     })
   );
 });
